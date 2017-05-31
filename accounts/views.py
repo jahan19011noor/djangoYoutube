e91b3cf84b4efd94e4ab6c5661e4096ab9976bbc
Noorjahan Mukammel <nmukammel@Noors-MacBook-Pro.local>
2017-05-31 22:45:55 +0600

    Tutorial 7 Completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return render(request, 'accounts/login.html')
+    numbers = [1,2,3,4,5]
+    name = "noor jahan mukammel"
+
+    args = {'name':name, 'numbers':numbers}
+
+    return render(request, 'accounts/home.html', args)

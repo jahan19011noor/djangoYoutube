51e4c0458cd63e5bba19c42d418609496d2f814b
Noorjahan Mukammel <nmukammel@Noors-MacBook-Pro.local>
2017-06-08 22:41:28 +0600

    Jinja2,StaticFiles,Urls,BaseTemplates

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home),
+    url(r'^login$', views.login),
+    url(r'^home$', views.home),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
-def home(request):
+def login(request):
     numbers = [1,2,3,4,5]
     name = "noor jahan mukammel"
 
     args = {'name':name, 'numbers':numbers}
 
-    return render(request, 'accounts/home.html', args)
+    return render(request, 'accounts/login.html', args)
+
+def home(request):
+    return render(request, 'accounts/home.html')

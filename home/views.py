7bd7f95601223d400b1bf6587f96f231cb768c61
Noorjahan Mukammel <nmukammel@Noors-MacBook-Pro.local>
2017-06-18 00:12:18 +0600

    django form vs html forms and Post requests

--- home/views.py
+++ home/views.py
@@ -1,21 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
-# function-based view example:
-
-# def home(request):
-#     return HttpResponse('It Works')
-
-
-# class-based view used:
-    # import generic class-based view and
-    # import TemplateView from there
-
+from home.forms import HomeForm
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
 
+    def get(self, request):
+        form = HomeForm()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+
+        # request.POST populates the form with the data received via the request
+        form = HomeForm(request.POST)
+
+        if form.is_valid():
+            text = form.cleaned_data['post']
+            form = HomeForm()
+
+            # redirects to the home page
+            # thus recreates the csrf token
+            # have to save data in database to display in this case.
+            return redirect('home:home')
+
+        args = {'form': form, 'text': text}
+        return render(request, self.template_name, args)
 
 # Note:
     # the TemplateView inherits from View class
@@ -27,4 +37,4 @@ class HomeView(TemplateView):
                 # which is handling the request explicitely
 
     # thus comes the get(), post(), put() etc methods:
-        # in the classes which are for the class-based views 
+        # in the classes which are for the class-based views

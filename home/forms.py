6a36025333836cc3574c70d8b4c331a5cf373832
Noorjahan Mukammel <nmukammel@Noors-MacBook-Pro.local>
2017-06-18 01:37:29 +0600

    modelForm, saving and retrieving from database and order_by: Tutorial 50

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,18 @@
 from django import forms
+from home.models import Post
 
-class HomeForm(forms.Form):
+class HomeForm(forms.ModelForm):
     post = forms.CharField()
+
+    class Meta:
+        model = Post
+
+        # the comma says to unpack tuple and keep it as tuple and not transfer it to a string
+        fields = ('post',)
+
+#         Note:
+#     we have not specified the User that has to be stored as well and that is not_empty by default
+#     gives an error as request gets submitted because user_id does not get saved
+
+#     We can save the user_id in the views.py
+
--- home/views.py
+++ home/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from home.forms import HomeForm
 from django.views.generic import TemplateView
+from home.models import Post
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
 
     def get(self, request):
         form = HomeForm()
-        return render(request, self.template_name, {'form':form})
+        posts = Post.objects.all().order_by('-created')
+
+        args = {'form':form, 'posts':posts}
+        return render(request, self.template_name, args)
 
     def post(self, request):
 
@@ -16,6 +20,15 @@ class HomeView(TemplateView):
         form = HomeForm(request.POST)
 
         if form.is_valid():
+            # .save() associates the form with the model
+            # as the Meta class says the model = Post
+            # form.save()
+
+            # rewrite .save() to save the user id
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
             text = form.cleaned_data['post']
             form = HomeForm()
 
@@ -24,8 +37,11 @@ class HomeView(TemplateView):
             # have to save data in database to display in this case.
             return redirect('home:home')
 
-        args = {'form': form, 'text': text}
-        return render(request, self.template_name, args)
+        # after redirect these line no longer load the data
+            # the data now has to be picked up from database and
+            # displayed using the get() method
+        # args = {'form': form, 'text': text}
+        # return render(request, self.template_name, args)
 
 # Note:
     # the TemplateView inherits from View class
